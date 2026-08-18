59f31efaea4114103a5334d00065a370d6a01f34
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2026-08-18 09:51:35 +0200

    feat(openurl): add BrowserLauncher abstraction over java.awt.Desktop

---
+++ src/main/kotlin/io/sentry/buddy/openurl/BrowserLauncher.kt
@@ -0,0 +1,17 @@
+package io.sentry.buddy.openurl
+
+import java.awt.Desktop
+import java.net.URI
+
+interface BrowserLauncher {
+    fun open(uri: URI)
+}
+
+class DesktopBrowserLauncher : BrowserLauncher {
+    override fun open(uri: URI) {
+        check(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            "Desktop browse action is not supported on this platform"
+        }
+        Desktop.getDesktop().browse(uri)
+    }
+}
